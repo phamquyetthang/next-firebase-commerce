f20786b34262d15ed3b2d650fb043c8eac01ac33
phamquyetthang <quyetthang.phucchi@gmail.com>
2024-04-20 12:13:37 +0700

    [#2]: default project structure

---
+++ src/utils/firebase.ts
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB__1E80qlI5TW3MQ0XT8ATOvk5kKtUWM4",
+  authDomain: "next-firebase-commerce.firebaseapp.com",
+  projectId: "next-firebase-commerce",
+  storageBucket: "next-firebase-commerce.appspot.com",
+  messagingSenderId: "282457970793",
+  appId: "1:282457970793:web:d497e35554ceba8815eaf8",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// firestore
+const db = getFirestore(app);
+
+// storage
+
+// authentication
+
+
+export { db };
